7fa9ed6c378d3e3a2cd19f257150dea1ae1a8003
Stéphane Bour <sbour@welcome-media.fr>
2017-12-08 22:36:53 +0100

    feat: add buckets aggregations
    [WIP]

--- kb/aggs.js
+++ kb/aggs.js
@@ -1,4 +1,6 @@
 sense.kb.addGlobalAutocompleteRules("aggregations", {
+
+        // Metrics Aggregations
         avg: {
             __template: {field: ""},
             field: "$FIELD$",
@@ -126,6 +128,51 @@ sense.kb.addGlobalAutocompleteRules("aggregations", {
                 __scope_link: "GLOBAL.SCRIPT_ENV"
             }
         },
+
+        // Buckets aggregation
+        date_histogram: {
+            __template: {field: "", interval: "month"},
+            interval: {__one_of: ["year", "quarter", "month", "week", "day", "hour", "minute", "1h", "1d", "1w"]},
+            field: "$FIELD$",
+            format: "",
+            offset: "",
+            keyed: {__one_of: [true, false]},
+            missing: "",
+            min_doc_count: ""
+        },
+        date_range: {
+            __template: {field: "", range: [{gte: "now-30d", lte: "now"}]},
+            field: "$FIELD$",
+            format: "",
+            range: [{gte: "", gt: "", lt: "", lte: "", to: "", from: ""}],
+        },
+        missing: {
+            __template: {field: ""},
+            field: "$FIELD$",
+        },
+        nested: {
+            __template: {path: "", aggs: {aggregation_name: {}}},
+            path: "$FIELD$",
+            aggs: {
+                "*": {
+                    __scope_link: "GLOBAL.aggregations"
+                }
+            },
+        },
+        terms: {
+            __template: {field: ""},
+            field: "$FIELD$",
+            size: "",
+            show_term_doc_count_error: {__one_of: [true, false]},
+            order: {
+                _count: {__one_of: ["desc", "asc"]},
+                _key: {__one_of: ["desc", "asc"]},
+                "*": {__one_of: ["desc", "asc"]}
+            }
+        },
+
+
+        // Loop
         aggs: {
             __template: {
                 "aggregation_name": {}
