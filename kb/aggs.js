026964ec26c42922a65f15de96a1bff7ddbd0f89
Stéphane Bour <sbour@welcome-media.fr>
2017-12-11 21:45:56 +0100

    chore: full autocomplete for buckets aggs

--- kb/aggs.js
+++ kb/aggs.js
@@ -146,48 +146,158 @@ sense.kb.addGlobalAutocompleteRules("aggregations", {
             format: "",
             range: [{gte: "", gt: "", lt: "", lte: "", to: "", from: ""}],
         },
-        missing: {
-            __template: {field: ""},
+        diversified_sampler: {
+            __template: {field: "", shard_size: ""},
+            shard_size: "",
             field: "$FIELD$",
+            max_docs_per_value: "",
+            script: {
+                __scope_link: "GLOBAL.SCRIPT_ENV"
+            }
         },
-        nested: {
-            __template: {path: "", aggs: {aggregation_name: {}}},
-            path: "$FIELD$",
-            aggs: {
+        filter: {
+            __scope_link: ".query"
+        },
+        filters: {
+            other_bucket_key: "",
+            filters: {
                 "*": {
-                    __scope_link: "GLOBAL.aggregations"
+                    __scope_link: ".query"
                 }
+            }
+        },
+        geo_distance: {
+            __template: {field: "", origin: ""},
+            origin: "",
+            field: "$FIELD$",
+            ranges: [{gte: "", gt: "", lt: "", lte: "", to: "", from: ""}],
+            unit: "",
+            distance_type: {__one_of: ["arc", "plane"]},
+            keyed: {__one_of: [true, false]},
+        },
+        global: {},
+        histogram: {
+            __template: {field: "", interval: "50"},
+            interval: "",
+            field: "$FIELD$",
+            offset: "",
+            keyed: {__one_of: [true, false]},
+            missing: "",
+            extended_bounds: {
+                min: "",
+                max: ""
             },
+            min_doc_count: ""
+        },
+        ip_range: {
+            __template: {field: "", ranges: [{to: ""}, {from: ""}]},
+            field: "$FIELD$",
+            ranges: [{to: "", from: "", mask: ""}],
+            keyed: {__one_of: [true, false]},
+        },
+        missing: {
+            __template: {
+                field: ""
+            }
+            ,
+            field: "$FIELD$",
+        }
+        ,
+        nested: {
+            __template: {
+                path: "", aggs:
+                    {
+                        aggregation_name: {}
+                    }
+            }
+            ,
+            path: "$FIELD$",
+            aggs:
+                {
+                    "*":
+                        {
+                            __scope_link: "GLOBAL.aggregations"
+                        }
+                }
+            ,
+        },
+        range: {
+            __template: {field: "", ranges: [{to: ""}, {from: ""}]},
+            field: "$FIELD$",
+            ranges: [{to: "", from: "", key: ""}],
+            keyed: {__one_of: [true, false]},
+            script: {
+                __scope_link: "GLOBAL.SCRIPT_ENV"
+            }
+        },
+        reverse_nested: {},
+        sampler: {
+            __template: {shard_size: "100"},
+            shard_size: ""
+        },
+        significant_terms: {
+            __template: {field: ""},
+            field: "$FIELD$"
+        },
+        significant_text: {
+            __template: {field: ""},
+            field: "$FIELD$"
         },
         terms: {
-            __template: {field: ""},
+            __template: {
+                field: ""
+            }
+            ,
             field: "$FIELD$",
-            size: "",
-            show_term_doc_count_error: {__one_of: [true, false]},
+            size:
+                "",
+            show_term_doc_count_error:
+                {
+                    __one_of: [true, false]
+                }
+            ,
             order: {
-                _count: {__one_of: ["desc", "asc"]},
-                _key: {__one_of: ["desc", "asc"]},
-                "*": {__one_of: ["desc", "asc"]}
-            }
-        },
-
-
-        // Loop
-        aggs: {
-            __template: {
-                "aggregation_name": {}
-            },
-            "*": {
-                __scope_link: "GLOBAL.aggregations"
-            }
-        },
-        aggregations: {
-            __template: {
-                "aggregation_name": {}
-            },
-            "*": {
-                __scope_link: "GLOBAL.aggregations"
+                _count: {
+                    __one_of: ["desc", "asc"]
+                }
+                ,
+                _key: {
+                    __one_of: ["desc", "asc"]
+                }
+                ,
+                "*":
+                    {
+                        __one_of: ["desc", "asc"]
+                    }
             }
         }
+        ,
+
+
+// Loop
+        aggs: {
+            __template: {
+                "aggregation_name":
+                    {}
+            }
+            ,
+            "*":
+                {
+                    __scope_link: "GLOBAL.aggregations"
+                }
+        }
+        ,
+        aggregations: {
+            __template: {
+                "aggregation_name":
+                    {}
+            }
+            ,
+            "*":
+                {
+                    __scope_link: "GLOBAL.aggregations"
+                }
+        }
     }
-);
+)
+;
