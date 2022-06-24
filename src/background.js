26d9647a0f40a3c202be4156424d8c60dcad4f74
Max Kovrigovich <max.kovrigovich@gmail.com>
2022-06-24 14:52:05 +0300

    migrate to manifest v3 (#24)
    Co-authored-by: Max V. Kovrigovich <maxim.kovrigovich@freshcells.de>

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.create({'url': chrome.extension.getURL('index.html')}, function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.tabs.create({'url': chrome.runtime.getURL('index.html')}, function (tab) {
   });
 });
